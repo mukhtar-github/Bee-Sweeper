a4861b558ab82000a1acd43f437dda610300083c
Mukhtar <mukhtartanimugarba@gmail.com>
2020-09-20 18:12:53 +0100

    File updated

--- sketch.js
+++ sketch.js
@@ -1,4 +1,26 @@
+
+function make2DArray(col, row) {
+    let arr = new Array(col);
+
+    for (let i = 0; i < arr.length; i++) {
+        arr[i] = new Array(row);
+    }
+}
+
 function Cell() {
     this.bee = true;
-    this.reveal = true;
+    this.revealed = true;
+}
+
+let grid;
+let col = 20;
+let row = 20;
+
+function setUp() {
+    createCanvas(200, 200);
+    grid = make2DArray(20, 20);
+}
+
+function draw() {
+
 }
