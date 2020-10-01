7547b539ea3afbebde7461c790c344598a4a95a4
Mukhtar <mukhtartanimugarba@gmail.com>
2020-10-01 11:11:06 +0100

    Files updated

--- cell.js
+++ cell.js
@@ -2,7 +2,11 @@ function Cell(x, y, w) {
     this.x = x;
     this.y = y;
     this.w = w;
-    this.bee = true;
+    if (random(1) < 0.5) {
+        this.bee = true;
+    } else {
+        this.bee = false;
+    }
     this.revealed = true;
 }
 
