69a86bd3ebe9a59f6cd521630fa3d6bbf0948f77
Mukhtar <mukhtartanimugarba@gmail.com>
2020-10-22 06:20:24 +0100

    File update

--- cell.js
+++ cell.js
@@ -54,4 +54,23 @@ Cell.prototype.contains = function(x, y) {
 
 Cell.prototype.reveal = function() {
     this.revealed = true;
+    if (this.neighborCount == 0) {
+        // flood fill time
+        this.floodFill();
+    }
+};
+
+Cell.prototype.floodFill = function() {
+    for (var xoff = -1; xoff <= 1; xoff++) {
+        for (var yoff = -1; yoff <= 1; yoff++) {
+            var i = this.i + xoff;
+            var j = this.j + yoff;
+            if (i > -1 && i < cols && j > -1 && j < rows) {
+                var neighbor = grid[i][j];
+                if (neighbor.bee) {
+                total++;
+                }   
+            }
+        }
+    }
 };
